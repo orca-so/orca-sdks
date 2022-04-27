073f3a63bb69637cb3298a777e4866544be9f3f6
Otto Cheung <otto@orca.so>
2022-04-26 17:25:35 -0700

    comments

--- src/web3/address-util.ts
+++ src/web3/address-util.ts
@@ -2,28 +2,22 @@ import { Address, translateAddress, utils } from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
 
 /**
- * @category Transactions Util
+ * @category Util
  */
 export type PDA = { publicKey: PublicKey; bump: number };
 
 /**
- * @category Transactions Util
+ * @category Util
  */
 export class AddressUtil {
   public static toPubKey(address: Address): PublicKey {
     return translateAddress(address);
   }
 
-  /**
-   * @category Transactions Util
-   */
   public static toPubKeys(addresses: Address[]): PublicKey[] {
     return addresses.map((address) => AddressUtil.toPubKey(address));
   }
 
-  /**
-   * @category Transactions Util
-   */
   public static findProgramAddress(
     seeds: (Uint8Array | Buffer)[],
     programId: PublicKey
