7a0f9b52ae77184feed901d6d0767f2e002d35a5
tmoc <michael@tmoclabs.com>
2023-05-04 09:22:27 +0800

    Renaming tx_limit constants

--- packages/common-sdk/src/web3/transactions/constants.ts
+++ packages/common-sdk/src/web3/transactions/constants.ts
@@ -1,8 +1,8 @@
 // The hard-coded limit of a transaction size in bytes
-export const TX_LIMIT = 1232;
+export const TX_SIZE_LIMIT = 1232;
 
 // The hard-coded limit of an encoded transaction size in bytes
-export const TX_ENCODED_LIMIT = 1644;
+export const TX_BASE64_ENCODED_SIZE_LIMIT = 1644;
 
 // A dummy blockhash to use for measuring transaction sizes
 export const MEASUREMENT_BLOCKHASH = {
