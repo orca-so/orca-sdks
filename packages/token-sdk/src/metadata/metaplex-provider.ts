882ed798adc97c2aceba55d32fbdc97b67094670
Wilhelm Thieme <wjthieme@gmail.com>
2023-10-27 11:27:44 +0200

    Remove dependency on metaplex library altogether

--- packages/token-sdk/src/metadata/metaplex-provider.ts
+++ packages/token-sdk/src/metadata/metaplex-provider.ts
@@ -1,11 +1,9 @@
 
-import { Connection } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { MetadataProvider, Metadata } from "./types";
 import { Address, AddressUtil } from "@orca-so/common-sdk";
 import PQueue from "p-queue";
-import { Context, publicKey } from "@metaplex-foundation/umi";
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { fetchJsonMetadata, fetchMetadata, fetchAllMetadata, findMetadataPda, Metadata as MetaplexMetadata, JsonMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { MetaplexClient, MetaplexHttpClient, OffChainMetadata } from "./client";
 
 const DEFAULT_CONCURRENCY = 5;
 const DEFAULT_INTERVAL_MS = 1000;
@@ -22,51 +20,91 @@ interface Opts {
 }
 
 export class MetaplexProvider implements MetadataProvider {
-  private readonly ctx: Context;
+  private readonly connection: Connection;
+  private readonly client: MetaplexClient;
   private readonly queue: PQueue;
   private readonly opts: Opts;
 
   constructor(connection: Connection, opts: Opts = {}) {
     const { concurrency = DEFAULT_CONCURRENCY, intervalMs = DEFAULT_INTERVAL_MS } = opts;
-    this.ctx = createUmi(connection.rpcEndpoint);
-
+    this.connection = connection;
+    this.client = new MetaplexHttpClient();
     this.queue = new PQueue({ concurrency, interval: intervalMs });
     this.opts = opts;
   }
 
   async find(address: Address): Promise<Readonly<Metadata> | null> {
-    try {
-      const pda = findMetadataPda(this.ctx, { mint: publicKey(address)});
-      const meta = await fetchMetadata(this.ctx, pda);
-      let image: string | undefined;
-      if (this.opts.loadImage) {
-        const json = await fetchJsonMetadata(this.ctx, meta.uri);
-        image = json.image;
-      }
-      return { symbol: meta.symbol, name: meta.name, image };
-    } catch (e) {
+    const pda = this.client.getMetadataAddress(new PublicKey(address));
+    const info = await this.connection.getAccountInfo(pda);
+    if (!info) {
       return null;
     }
+    const meta = this.client.parseOnChainMetadata(pda, info.data);
+    if (!meta) {
+      return null;
+    }
+    let image: string | undefined;
+    if (this.opts.loadImage) {
+      const json = await this.client.getOffChainMetadata(meta);
+      if (json) {
+        image = json.image;
+      }
+    }
+    return { symbol: meta.symbol, name: meta.name, image };
   }
 
   async findMany(addresses: Address[]): Promise<ReadonlyMap<string, Metadata | null>> {
     const mints = AddressUtil.toPubKeys(addresses);
-    const pdas = mints.map((mint) => findMetadataPda(this.ctx, { mint: publicKey(mint)}));
-    const metas = await fetchAllMetadata(this.ctx, pdas);
-    let jsons = Array<JsonMetadata | undefined>(metas.length);
-    if (this.opts.loadImage) {
-      jsons = await this.queue.addAll(
-        metas.map((meta) => async () => fetchJsonMetadata(this.ctx, meta.uri))
-      );
+    const pdas = mints.map((mint) => this.client.getMetadataAddress(mint));
+
+    // chunk the requests into groups of 100 since this is the max number of accounts
+    // that can be fetched in a single request using `getMultipleAccountsInfo`
+    let datas = Array<Buffer | null>(pdas.length);
+    const dataHandlers = Array<() => Promise<void>>();
+    const chunkSize = 100;
+    for (let i = 0; i < pdas.length; i += chunkSize) {
+      const chunk = pdas.slice(i, i + chunkSize);
+      dataHandlers.push(async () => {
+        const chunkInfos = await this.connection.getMultipleAccountsInfo(chunk);
+        for (let j = 0; j < chunkInfos.length; j++) {
+          datas[i + j] = chunkInfos[j]?.data ?? null;
+        }
+      });
     }
 
-    return new Map(
-      metas.map((meta, index) => {
-        const mint = mints[index].toBase58();
-        const json = jsons[index];
-        const result = { symbol: meta.symbol, name: meta.name, image: json?.image };
-        return [mint, result];
-      })
-    );
+    await this.queue.addAll(dataHandlers);
+
+    const metas = datas.map((data, index) => data ? this.client.parseOnChainMetadata(pdas[index], data) : null);
+    let jsons = Array<OffChainMetadata | null>(metas.length);
+    const jsonHandlers = Array<() => Promise<void>>();
+    if (this.opts.loadImage) {
+      for (let i = 0; i < metas.length; i += 1) {
+        const meta = metas[i];
+        if (!meta) {
+          continue;
+        }
+        jsonHandlers.push(async () => {
+          const json = await this.client.getOffChainMetadata(meta);
+          jsons[i] = json;
+        });
+      }
+    }
+
+    await this.queue.addAll(jsonHandlers);
+
+    const map = new Map<string, Metadata | null>();
+
+    for (let i = 0; i < pdas.length; i += 1) {
+      const mint = mints[i];
+      const meta = metas[i];
+      const json = jsons[i];
+      if (!meta) {
+        continue;
+      }
+      const result = { symbol: meta.symbol, name: meta.name, image: json?.image };
+      map.set(mint.toString(), result);
+    }
+
+    return map;
   }
 }
