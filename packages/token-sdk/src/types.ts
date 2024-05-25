ed1095402a7a8bb534c0df25e9eb7b5f6629959d
yugure-orca <109891005+yugure-orca@users.noreply.github.com>
2024-05-25 09:36:56 +0900

    token sdk update to handle token-2022 tokens (#83)
    * add Token-2022 as token program
    
    * package bump
    
    * update BasicSupportedTypes
    
    * bump 0.6.0-alpha.2
    
    * use common-sdk with TokenExtension
    
    * bump to 0.4.0-alpha.0

--- packages/token-sdk/src/types.ts
+++ packages/token-sdk/src/types.ts
@@ -1,7 +1,7 @@
 import { Address } from "@orca-so/common-sdk";
 import { Metadata } from "./metadata";
 
-export type Token = { mint: Address; decimals: number } & Metadata;
+export type Token = { mint: Address; decimals: number, tokenProgram: Address } & Metadata;
 
 export interface Mintlist {
   name: string;
--- packages/token-sdk/tests/metaplex-provider.test.ts
+++ packages/token-sdk/tests/metaplex-provider.test.ts
@@ -11,8 +11,10 @@ describe("metaplex-provider", () => {
   // ORCA has metadata, but not has image info, so use BONK and SHDW
   const MINT_BONK = "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263";
   const MINT_SHDW = "SHDWyBxihqiCj6YekG2GUr7wqKLeLAMK1gHZck9pL6y";
+  // Token-2022
+  const MINT_BERN_2022 = "CKfatsPMUf8SkiURsDXs7eK6GWb4Jsd6UDbs7twMCWxo";
 
-  it("find ok", async () => {
+  it("find ok (Token)", async () => {
     const timeoutMs = 2000;
     const metaplex_provider = new MetaplexProvider(ctx.connection, {loadImage: true, concurrency: 1, intervalMs: 1000, getOffChainMetadataTimeoutMs: timeoutMs});
     const fetcher = new TokenFetcher(ctx.connection).addProvider(metaplex_provider);
@@ -26,12 +28,30 @@ describe("metaplex-provider", () => {
     expect(result.image).toEqual("https://arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I");
   });
 
-  it("findMany ok", async () => {
+  it("find ok (Token-2022)", async () => {
+    const timeoutMs = 2000;
+    const metaplex_provider = new MetaplexProvider(ctx.connection, {loadImage: true, concurrency: 1, intervalMs: 1000, getOffChainMetadataTimeoutMs: timeoutMs});
+    const fetcher = new TokenFetcher(ctx.connection).addProvider(metaplex_provider);
+
+    const result = await fetcher.find(MINT_BERN_2022, true);
+    expect(result).toBeDefined();
+    expect(result.mint).toEqual(MINT_BERN_2022);
+    expect(result.decimals).toEqual(5);
+    // Metadata address for BERN is 6mbNeu9pSERJrJVuPiPyZCm1PPUw2CLBGmdn6dW75azw
+    // But it is not one owned by Metaplex Token Metadata program (metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s).
+    // Its owner program is META4s4fSmpkTbZoUsgC1oBnWB31vQcmnN8giPw51Zu (I cannot find the info about this program, but I guess it is provided by FlexBeam team)
+    // Many Token-2022 tokens has no Metaplex metadata, so I expect to resolve their metadata by other providers.
+    expect(result.name).toBeUndefined();
+    expect(result.symbol).toBeUndefined();
+    expect(result.image).toBeUndefined();
+  });
+
+  it("findMany ok (Token + Token-2022)", async () => {
     const timeoutMs = 2000;
     const metaplex_provider = new MetaplexProvider(ctx.connection, {loadImage: true, concurrency: 2, intervalMs: 1000, getOffChainMetadataTimeoutMs: timeoutMs});
     const fetcher = new TokenFetcher(ctx.connection).addProvider(metaplex_provider);
 
-    const result = await fetcher.findMany([MINT_BONK, MINT_SHDW], true);
+    const result = await fetcher.findMany([MINT_BONK, MINT_SHDW, MINT_BERN_2022], true);
     const resultBonk = result.get(MINT_BONK);
     expect(resultBonk).toBeDefined();
     expect(resultBonk?.mint).toEqual(MINT_BONK);
@@ -47,6 +67,15 @@ describe("metaplex-provider", () => {
     expect(resultShdw?.name).toEqual("Shadow Token");
     expect(resultShdw?.symbol).toEqual("SHDW");
     expect(resultShdw?.image).toEqual("https://shdw-drive.genesysgo.net/FDcC9gn12fFkSU2KuQYH4TUjihrZxiTodFRWNF4ns9Kt/250x250_with_padding.png");
+
+    const resultBern = result.get(MINT_BERN_2022);
+    expect(resultBern).toBeDefined();
+    expect(resultBern?.mint).toEqual(MINT_BERN_2022);
+    expect(resultBern?.decimals).toEqual(5);
+    // see: comments on "find ok (Token-2022)" test
+    expect(resultBern?.name).toBeUndefined();
+    expect(resultBern?.symbol).toBeUndefined();
+    expect(resultBern?.image).toBeUndefined();
   });
 
   it("find timeout", async () => {
