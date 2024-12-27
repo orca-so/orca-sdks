95729a7bc7e7d904cd63697103625ceb52efa64d
Will <82029448+wjthieme@users.noreply.github.com>
2024-12-27 10:26:13 +0100

    Remove common-sdk from this repo (#95)

--- packages/common-sdk/src/web3/transactions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./compute-budget";
-export * from "./constants";
-export * from "./transactions-builder";
-export * from "./transactions-processor";
-export * from "./types";
--- packages/common-sdk/tests/utils/expectations.ts
+++
@@ -1,9 +0,0 @@
-import { Mint } from "@solana/spl-token";
-
-export function expectMintEquals(actual: Mint, expected: Mint) {
-  expect(actual.decimals).toEqual(expected.decimals);
-  expect(actual.isInitialized).toEqual(expected.isInitialized);
-  expect(actual.mintAuthority!.equals(expected.mintAuthority!)).toBeTruthy();
-  expect(actual.freezeAuthority!.equals(expected.freezeAuthority!)).toBeTruthy();
-  expect(actual.supply === expected.supply).toBeTruthy();
-}
